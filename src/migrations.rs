5d3a03243d91dd3066229ef79f725b1d30141a38
jakob <lilliemarckjakob@gmail.com>
2025-11-16 08:56:58 +0100

    fix: remove redundant schema creation, and remove static type checking in migrator - thats a chicken and egg situation

--- src/migrations.rs
+++ src/migrations.rs
@@ -21,13 +21,8 @@ where
 {
     let mut tx = conn.begin().await?;
 
-    // Ensure the 'fx_event_bus' schema exists
-    sqlx::query!("CREATE SCHEMA IF NOT EXISTS fx_event_bus;")
-        .execute(&mut *tx)
-        .await?;
-
     // Temporarily set search_path for this transaction
-    sqlx::query!("SET LOCAL search_path TO fx_event_bus;")
+    sqlx::query("SET LOCAL search_path TO fx_event_bus;")
         .execute(&mut *tx)
         .await?;
 
